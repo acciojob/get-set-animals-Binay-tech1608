95959357c223939e6bd08e041ad5a555c1818430
Binay Mann <82080713+Binay-tech1608@users.noreply.github.com>
2026-05-13 12:41:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ class Animal {
 	constructor(species){
 		this._species = species;
 	}
+	get species(){
+	return this._species;
+	}
 	makeSound(){
 		console.log(`The ${this.species} makes a sound`);
 	}
